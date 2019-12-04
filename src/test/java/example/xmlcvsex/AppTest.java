88a7c6c468ccfe46d5057b551b2734d631adae9e
손병록 <brson@hotmail.com>
2019-12-04 11:52:10 +0900

    test

---
+++ src/test/java/example/xmlcvsex/AppTest.java
@@ -0,0 +1,15 @@
+package example.xmlcvsex;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
